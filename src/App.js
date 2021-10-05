7ffafbc5daf941b7e087c7945328a9b45db69c07
AndersonS <klaito.b@gmail.com>
2021-10-05 09:04:43 -0300

    switch light/dark mode

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { useCallback, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { themes } from './themes';
+import { AppContainer, MainSection } from './components/app.style';
+import { Button } from './components/button.style';
+
+function App() {
+
+  const [currentTheme, setCurrentTheme]=useState("light")
+
+  const getOpositeTheme = useCallback(()=>(currentTheme==="light"?"dark":"light"), [currentTheme])
+
+  return (
+    <ThemeProvider theme={themes[currentTheme]}>
+<AppContainer>
+  <MainSection>
+    <h1>You are in {currentTheme} mode</h1>
+    <Button onClick={()=>setCurrentTheme(getOpositeTheme)}>Switch to {getOpositeTheme()} mode</Button>
+  </MainSection>
+</AppContainer>
+    </ThemeProvider>
+  );
+}
+
+export default App;
